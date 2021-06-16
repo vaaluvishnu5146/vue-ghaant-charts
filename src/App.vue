9330cafe2ee9e09674e8f52d035fdd908504f0e0
vishnu vardhan <vaaluvishnu5146@gmail.com>
2021-06-16 18:10:29 +0530

    Line generate

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <timeline ref="timeline" :items="items" :groups="groups" :options="options">
+    </timeline>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Timeline, DataSet } from "vue2vis";
+import "vue2vis/dist/vue2vis.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Timeline,
+  },
+  methods: {},
+  data() {
+    return {
+      groups: new DataSet([
+        {
+          id: 0,
+          content: "Software",
+        },
+        {
+          id: 1,
+          content: "Chip Level",
+        },
+        {
+          id: 2,
+          content: "Critical",
+        },
+        {
+          id: 3,
+          content: "Testing",
+        },
+      ]),
+      items: new DataSet([
+        {
+          id: 0,
+          group: 2,
+          start: new Date(),
+          content: "",
+        },
+      ]),
+      options: {
+        orientation: "top",
+        type: "point",
+      },
+    };
+  },
+  created() {},
+  mounted() {
+    var horizontalLine = document.createElement("div");
+    horizontalLine.className = "horizontal-line";
+    var container = document.querySelector(".vis-vertical");
+    container.appendChild(horizontalLine);
+  },
+};
 </script>
 
 <style>
@@ -24,5 +67,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.horizontal-line {
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background: #2c3e50;
+  z-index: 1;
+  top: 130px;
 }
 </style>
